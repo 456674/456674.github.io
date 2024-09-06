3f5ed48f96a0e115e0095e3008566e4b39556809
456674 <61876073+456674@users.noreply.github.com>
2024-09-06 16:20:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('The website is fully loaded.');
+
+    const imageElement = document.getElementById('myImage');
+    const image1 = 'image1.jpg';
+    const image2 = 'image2.jpg';
+
+    imageElement.addEventListener('click', function() {
+        if (imageElement.src.includes(image1)) {
+            imageElement.src = image2;
+        } else {
+            imageElement.src = image1;
+        }
+    });
 });
