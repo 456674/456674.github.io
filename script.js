3cea03cb2f25145b4e049432a08eaf1636aa86b8
456674 <61876073+456674@users.noreply.github.com>
2024-09-17 16:48:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('The website is fully loaded.');
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
 
-    const imageElement = document.getElementById('myImage');
-    const image1 = 'image1.jpeg';
-    const image2 = 'image2.jpg';
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
 
-    imageElement.addEventListener('click', function() {
-        if (imageElement.src.includes(image1)) {
-            imageElement.src = image2;
-        } else {
-            imageElement.src = image1;
-        }
-    });
+/* Declaring the array of image filenames */
+
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+const alts = {
+  'pic1.jpg' : 'Closeup of a human eye',
+  'pic2.jpg' : 'Rock that looks like a wave',
+  'pic3.jpg' : 'Purple and white pansies',
+  'pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
+  'pic5.jpg' : 'Large moth on a leaf'
+}
+
+/* Looping through images */
+
+for (const image of images) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${image}`);
+  newImage.setAttribute('alt', alts[image]);
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener('click', e => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
+}
+
+/* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+  const btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
 });
